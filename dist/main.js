026093fcbbb1fd44bd7ba08e5137a13f1d5da76e
rowan <nguyentuanamit@gmail.com>
2026-01-29 13:35:44 +0700

    u[pdate

--- dist/main.js
+++ dist/main.js
@@ -8,11 +8,7 @@ const exercise_templates_seed_1 = require("./database/seeds/exercise-templates.s
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: [
-            'http://localhost:3000',
-            'http://localhost:3001',
-            'http://localhost:3003',
-        ],
+        origin: ['*'],
         credentials: true,
     });
     app.useGlobalPipes(new common_1.ValidationPipe({
--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,7 @@ async function bootstrap() {
 
   // Enable CORS for frontend
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'http://localhost:3003',
-    ],
+    origin: ['*'],
     credentials: true,
   });
 
